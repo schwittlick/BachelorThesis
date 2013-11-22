816458a490f3edfb56e716047663e84679d89a26
Marcel Schwittlick <marzzzel@gmail.com>
2013-11-22 23:14:04 +0100

    makeBinary is now not static anymore. object orientation, here we come
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/ip/StandardImageProcessor.cpp
+++ src/ip/StandardImageProcessor.cpp
@@ -20,48 +20,11 @@ void StandardImageProcessor::makeBinary( cv::gpu::GpuMat * gpuImage )
 	cv::erode( im, im, cv::Mat(), cv::Point( -1, -1 ), 1, 1, 1 );
 	cv::threshold( im, im, 100, 255, 0 );
 	std::cout << "finished eroding, setting up blober " << std::endl;
+
 	
-	BlobDetector det;
 	det.detect( &im );
-	
-	//std::cout << "Found " << keypoints.size() << " blobs." << std::endl;
-
-	//cv::drawKeypoints( im, keypoints, im );
-
-	//cv::imwrite( "image_blobbed_2.jpg", im );
-	
+	//points = det.detect( &im );
+	std::cout << "finished detection." << std::endl;
+	gpuImage->upload( im );
 	std::cout << "done drawing keypoints" << std::endl;
-	
-	/*
-	//cv::cvtColor( im, im, CV_GRAY2BGR );
-
-	std::cout << "Found " << keypoints.size() << " blobs." << std::endl;
-	
-	for (int i=0; i<keypoints.size(); i++){
-		float X=keypoints[i].pt.x; 
-		float Y=keypoints[i].pt.y;
-		cv::rectangle( im, cv::Rect( X, Y, 10, 10 ), cv::Scalar( 255, 0, 0 ), 4 );
-		//cv::rectangle()
-	}
-	//cv::imwrite( "image_blobbed.jpg", im );
-	std::cout << "finished drawing blobs." << std::endl;
-	
-	gpuImage->upload( im );
-	std::cout << "type im: " << im.type() << std::endl;
-
-	*/
-	//delete blob_detect;
-	gpuImage->upload( im );
-	std::cout << "Finished uploading the image back onto the gpu image." << std::endl;
-	//delete blob_detect;
-	std::cout << "finished deleting detector" << std::endl;
-	//return im;
-	//gpuImage->upload( im );
-	//std::cout << "Finished uploading the image back onto the gpu image." << std::endl;
-
-	//std::cout << "Tyoe if gpuimage: " << gpuImage->type() << std::endl;
-	//cv::gpu::cvtColor( *gpuImage, *gpuImage, CV_RGBA2GRAY );
-	//std::cout << "Tyoe if gpuimage: " << gpuImage->type() << std::endl;
-	//cv::gpu::threshold( *gpuImage, *gpuImage, 100, cv::THRESH_BINARY, cv::THRESH_BINARY );
-	//std::cout << "Finished binary." << std::endl;
 }
--- src/ip/StandardImageProcessor.h
+++ src/ip/StandardImageProcessor.h
@@ -3,10 +3,13 @@
 #include < iostream >
 #include < vector >
 
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
 #include "opencv2/gpu/gpu.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
 
 #include "src/detect/BlobDetector.h"
 
@@ -16,6 +19,8 @@ public:
 	StandardImageProcessor(void);
 	~StandardImageProcessor(void);
 
-	static void makeBinary( cv::gpu::GpuMat * gpuImage );
+	void makeBinary( cv::gpu::GpuMat * gpuImage );
+	BlobDetector det;
+	std::vector< cv::KeyPoint > points;
 };
 
