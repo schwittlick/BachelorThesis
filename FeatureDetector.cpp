5a23b604285168679f48d07c3817894e160a2b7d
Marcel Schwittlick <marzzzel@gmail.com>
2013-11-22 20:59:05 +0100

    adds the barebones of the FeatureDetector class
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- FeatureDetector.cpp
+++ FeatureDetector.cpp
@@ -14,3 +14,38 @@ void FeatureDetector::processGoodFeaturesToTrack_CPU( cv::Mat * _image )
 {
 
 }
+
+void FeatureDetector::processGoodFeaturesToTrack_GPU( cv::gpu::GpuMat * _gpu_image )
+{
+
+}
+
+void FeatureDetector::processFASTFeatureDetection_GPU( cv::Mat * _image )
+{
+
+}
+
+void FeatureDetector::processHarrisCornerDetection_CPU( cv::Mat * _image )
+{
+
+}
+
+void FeatureDetector::processSURFFeatureDetection_GPU( cv::Mat * _image )
+{
+
+}
+
+void FeatureDetector::processSurf_OCL_FeatureDetection_GPU( cv::Mat * _image )
+{
+
+}
+
+void FeatureDetector::processSIFTFeatureDetection_GPU( cv::Mat * _image )
+{
+
+}
+
+void FeatureDetector::processPreCornerDetect( cv::Mat * _image )
+{
+
+}
