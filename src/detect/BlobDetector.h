ee1dd0083bfe7aa3221cfe240a751847d3a32f62
Marcel Schwittlick <marzzzel@gmail.com>
2013-11-23 00:14:21 +0100

    adds some includes to the blobdetector so it might work.
    Signed-off-by: Marcel Schwittlick <marzzzel@gmail.com>

--- src/detect/BlobDetector.h
+++ src/detect/BlobDetector.h
@@ -8,7 +8,8 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <opencv2/objdetect/objdetect.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/legacy/blobtrack.hpp"
 
 
 
