34758961daef6b1851ea08f73479369099d857e6
mrzl <marzzzel@gmail.com>
2013-12-10 23:29:20 +0100

    adds a gui to the GradientStep #80
    Signed-off-by: mrzl <marzzzel@gmail.com>

--- src/ip/steps/GradientStep.cpp
+++ src/ip/steps/GradientStep.cpp
@@ -1,13 +1,19 @@
 #include "GradientStep.h"
 
 
-GradientStep::GradientStep(void)
+GradientStep::GradientStep( QWidget *parent ) :
+	iterations( 1 )
 {
+	controls = new GradientStepDialog( this );
+
+	connect( controls, SIGNAL( kernelSizeChanged( int ) ), this, SLOT( kernelSizeChanged( int ) ) );
+	connect( controls, SIGNAL( iterationsChanged( int ) ), this, SLOT( iterationsChanged( int ) ) );
 }
 
 
 GradientStep::~GradientStep(void)
 {
+	delete controls;
 }
 
 void GradientStep::apply( cv::gpu::GpuMat * image )
@@ -16,8 +22,23 @@ void GradientStep::apply( cv::gpu::GpuMat * image )
 	{
 		cv::Mat tmp;
 		image->download( tmp );
-		cv::morphologyEx( tmp, tmp, CV_MOP_GRADIENT, getKernel(), cv::Point( getKernelSize(), getKernelSize() ), 1 );
+		cv::morphologyEx( tmp, tmp, CV_MOP_GRADIENT, getKernel(), cv::Point( getKernelSize(), getKernelSize() ), iterations );
 		image->upload( tmp );
 		//cv::gpu::morphologyEx( *image, *image, cv::MORPH_GRADIENT, kernel, cv::Point( -1, -1 ), 1 );
 	}
+}
+
+void GradientStep::toggleConfigWindow( void )
+{
+	controls->setVisible( !controls->isVisible() );
+}
+
+void GradientStep::iterationsChanged( int _it )
+{
+	this->iterations = _it;
+}
+
+void GradientStep::kernelSizeChanged( int _k )
+{
+	this->setKernelSize( _k );
 }
--- src/ip/steps/GradientStep.h
+++ src/ip/steps/GradientStep.h
@@ -1,13 +1,25 @@
 #pragma once
 
 #include "ProcessingStep.h"
+#include "QObject"
+#include "../dialogs/gradientstepdialog.h"
 
 class GradientStep : public ProcessingStep
 {
+	Q_OBJECT
 public:
-	GradientStep(void);
+	GradientStep( QWidget *parent = 0 );
 	~GradientStep(void);
 
 	void apply( cv::gpu::GpuMat * image );
+	void toggleConfigWindow( void );
+
+public slots:
+	void iterationsChanged( int _it );
+	void kernelSizeChanged( int _k );
+
+private:
+	GradientStepDialog * controls;
+	int iterations;
 };
 
