825a6f4aeb02e5420fd5a2addd5f5d86616e14f9
mrzl <marzzzel@gmail.com>
2013-12-12 20:07:05 +0100

    mintor restructuring in the slot/signal handling
    Signed-off-by: mrzl <marzzzel@gmail.com>

--- src/opticalflow/dialogs/OpticalFlowFarnebackConfigDialog.cpp
+++ src/opticalflow/dialogs/OpticalFlowFarnebackConfigDialog.cpp
@@ -5,14 +5,14 @@ OpticalFlowFarnebackConfigDialog::OpticalFlowFarnebackConfigDialog(QWidget *pare
 {
 	ui.setupUi(this);
 
-	connect( ui.slider_winSize, SIGNAL( valueChanged( int ) ), parent, SLOT( changedWinSize( int ) ) );
-	connect( ui.slider_iterations, SIGNAL( valueChanged( int ) ), parent, SLOT( changedIterations( int ) ) );
-	connect( ui.slider_levels, SIGNAL( valueChanged( int ) ), parent, SLOT( changedLevels( int ) ) );
-	connect( ui.slider_poly_n, SIGNAL( valueChanged( int ) ), parent, SLOT( changedPolyN( int ) ) );
-	connect( ui.slider_poly_sigma, SIGNAL( valueChanged( int ) ), parent, SLOT( changedPolySigma( int ) ) );
-	connect( ui.slider_pyr_scale, SIGNAL( valueChanged( int ) ), parent, SLOT( changedPyrScale( int ) ) );
-	connect( ui.slider_stepSize, SIGNAL( valueChanged( int ) ), parent, SLOT( changedStepSize( int ) ) );
-	connect( ui.slider_scale, SIGNAL( valueChanged( int ) ), parent, SLOT( changedScale( int ) ) );
+	connect( ui.slider_winSize, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedWinSize( int ) ) );
+	connect( ui.slider_iterations, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedIterations( int ) ) );
+	connect( ui.slider_levels, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedLevels( int ) ) );
+	connect( ui.slider_poly_n, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedPolyN( int ) ) );
+	connect( ui.slider_poly_sigma, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedPolySigma( int ) ) );
+	connect( ui.slider_pyr_scale, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedPyrScale( int ) ) );
+	connect( ui.slider_stepSize, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedStepSize( int ) ) );
+	connect( ui.slider_scale, SIGNAL( valueChanged( int ) ), this, SIGNAL( changedScale( int ) ) );
 }
 
 OpticalFlowFarnebackConfigDialog::~OpticalFlowFarnebackConfigDialog()
--- src/opticalflow/dialogs/OpticalFlowFarnebackConfigDialog.h
+++ src/opticalflow/dialogs/OpticalFlowFarnebackConfigDialog.h
@@ -12,7 +12,15 @@ public:
 	OpticalFlowFarnebackConfigDialog(QWidget *parent = 0);
 	~OpticalFlowFarnebackConfigDialog();
 
-
+signals:
+	void changedWinSize( int _winsize );
+	void changedIterations( int _it );
+	void changedLevels( int _lev );
+	void changedPolyN( int _polyn );
+	void changedPolySigma( int _polysigma );
+	void changedPyrScale( int _pyrscale );
+	void changedStepSize( int _stepsize );
+	void changedScale( int _scale );
 private:
 	Ui::OpticalFlowFarnebackConfigDialog ui;
 };
