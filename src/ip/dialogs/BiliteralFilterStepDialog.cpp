2263fa196434eea1976e8e13f280aa298b613821
mrzl <marzzzel@gmail.com>
2013-12-14 00:38:10 +0100

    fixes the gui for the biliteralfilterstep + #79
    Signed-off-by: mrzl <marzzzel@gmail.com>

--- src/ip/dialogs/BiliteralFilterStepDialog.cpp
+++ src/ip/dialogs/BiliteralFilterStepDialog.cpp
@@ -6,7 +6,7 @@ BiliteralFilterStepDialog::BiliteralFilterStepDialog(QWidget *parent)
 	ui.setupUi(this);
 
 	connect( ui.dSlider, SIGNAL( valueChanged( int ) ), this, SIGNAL( dChanged( int ) ) );
-	connect( ui.borderTypeSlider, SIGNAL( valueChanged( int ) ), this, SIGNAL( borderTypeChanged( int ) ) );
+	//connect( ui.borderTypeSlider, SIGNAL( valueChanged( int ) ), this, SIGNAL( borderTypeChanged( int ) ) );
 	connect( ui.sigmaColorSlider, SIGNAL( valueChanged( int ) ), this, SIGNAL( sigmaColorChanged( int ) ) );
 	connect( ui.sigmaSpaceSlider, SIGNAL( valueChanged( int ) ), this, SIGNAL( sigmaSpaceChanged( int ) ) );
 }
