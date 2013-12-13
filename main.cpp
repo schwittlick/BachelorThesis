f4b303e9b9195c55b81d3705522308c00b400af9
mrzl <marzzzel@gmail.com>
2013-12-14 00:43:21 +0100

    adds memory leak detector to main
    Signed-off-by: mrzl <marzzzel@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,10 +3,27 @@
 #include <thread>
 #include "src/misc/Console.h"
 
+//memory leak detectio
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#define _CRTDBG_MAP_ALLOC 
+#define _CRTDBG_MAP_ALLOC_NEW
+
+#ifdef _DEBUG   
+#ifndef DBG_NEW      
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )      
+#define new DBG_NEW   
+#endif
+#endif  // _DEBUG
+
 int main(int argc, char *argv[])
 {
+	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_DEBUG );
+
 	QApplication a(argc, argv);
-	
+
 	Console::createConsole();
 
 	BachelorThesis w;
